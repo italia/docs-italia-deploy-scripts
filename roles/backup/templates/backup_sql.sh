c0d5bdc3d6028686fd84a1cbcf3bd68ca173f3d9
Iacopo Spalletti <i.spalletti@nephila.it>
2019-11-02 13:16:22 +0100

    Fix downloading backup scripts

--- roles/backup/templates/backup_sql.sh
+++ roles/backup/templates/backup_sql.sh
@@ -5,7 +5,9 @@ set -e
 DATE=`date +"%Y%m%d-%H%M%S"`
 BACKUP_FILE="{{ backup_dir }}/docs-${DATE}.sql.gz"
 
-find {{ backup_dir }} -ctime +{{ backup_history }}
+if [[ "$1" == "delete" ]]; then
+  find {{ backup_dir }} -ctime +{{ backup_history }} -delete
+fi
 
 pg_dump -h {{ rtd_db_host|default('localhost', true) }} -p {{ rtd_db_port }} -U {{ rtd_db_user }} {{ rtd_db_name }} | gzip -9c > $BACKUP_FILE
 
--- roles/backup/templates/backup_ssl.sh
+++ roles/backup/templates/backup_ssl.sh
@@ -5,7 +5,9 @@ set -e
 DATE=`date +"%Y%m%d-%H%M%S"`
 BACKUP_FILE="{{ backup_dir }}/docs-ssl-${DATE}.tar.gz"
 
-find {{ backup_dir }} -ctime +{{ backup_history }}
+if [[ "$1" == "delete" ]]; then
+  find {{ backup_dir }} -ctime +{{ backup_history }} -delete
+fi
 
 cd /etc
 tar -czf ${BACKUP_FILE} letsencrypt
